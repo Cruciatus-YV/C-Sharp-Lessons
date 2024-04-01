44f72c8dd4da32ea029b277e47ea8b2cb479b5bf
Cruciatus-YV <voronkin_yura@list.ru>
2024-04-01 18:42:03 +0300

    ClassCreate

---
+++ TestApp/ClassCreate/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace TestApp.ClassCreate
+{
+    public class Student
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public byte? Course { get; set; }
+        public bool IsOnBudget { get; set; }
+        public Student() { }
+        public Student(string name, bool isOnBudget, byte? course = null, Guid? id = null)
+        {
+            Name = name;
+            IsOnBudget = isOnBudget;
+            Course = course;
+            if (id.HasValue)
+            {
+                Id = id.Value;
+            }
+        }
+        public override string ToString() => new StringBuilder()
+                .AppendLine($"Studen's id: {Id}")
+                .AppendLine($"Full name: {Name}")
+                .AppendLine($"Course: {Course}")
+                .AppendLine($"Budget: {IsOnBudget}")
+                .ToString();
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Fuck you, World!");
+﻿using TestApp.ClassCreate;
+
+Console.WriteLine("Hello, World!\n");
+var studentsList = new List<Student>()
+{
+    new ("Voronkin Yuriy Aleksandrovich", false, 5),
+    new ("Gryn Bogdan Vasilievich", false, 4),
+    new ()
+    {
+        Name = "Artem pohui",
+        Course = 3,
+        IsOnBudget = true,
+    }
+};
+studentsList.ForEach(student => Console.WriteLine(student.ToString()));
+Console.ReadLine();
